d0092720269f779bf6ee6017f6ea6f849da7b573
eanekwe <eane0001@student.monash.edu>
2024-12-10 21:10:18 +1100

    edit: settings

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+# core settings
+
+class Settings:
+    def __init__(self, gridSize, agents, obstacles, food):
+        self.gridSize = 10
+        self.agents = 1
+        self.obstacles = 1
+        self.food = 1
+    
