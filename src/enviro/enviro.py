6dcb78139bdd2a6a3185d67f2ce07abb43e48f98
eanekwe <eane0001@student.monash.edu>
2024-12-12 13:03:10 +1100

    edit: settings, add: enviro.py

---
+++ src/enviro/enviro.py
@@ -0,0 +1,16 @@
+import numpy as np
+from config import settings
+
+class Enviro:
+    def __init__(self, settings):
+        self.grid_size = settings.grid_size
+        # instantiate 2d grid
+        self.grid = np.zeroes(self.grid_size)
+        self.agents = []
+    
+    def reset(self):
+        self.grid = (0)
+        self.agents.clear()
+    
+    def update(self):
+        pass
