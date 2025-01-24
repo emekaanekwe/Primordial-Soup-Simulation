584f33e5fb5341082bc5517fbf43727e55e43ad5
emekaenshinyan <emeka.enshinyan@gmail.com>
2025-01-24 22:10:42 +1100

    edit: grid

--- sandbox.py
+++ sandbox.py
@@ -1,16 +1,9 @@
-from numpy import random
-
-
-
-
-    
-    
 def make_gene():
     gene = []
     #assume 64 entriesof code
     entries = 64
     while len(gene) < entries:
-        n = random.randint(0,3)
+
         if n == 0:
             gene.append("A")
         elif n == 1:
